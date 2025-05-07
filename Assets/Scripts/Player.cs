15d44b9693aed1cb89249ce24164a8c2ca5a8782
GreedSpirit <dlwjdgus0918@naver.com>
2025-05-07 23:00:51 +0900

    Player InputSystem

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Vector2 inputVec;
+    [SerializeField] float speed;
+
+    Rigidbody2D rigid;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value){
+        inputVec = value.Get<Vector2>();
+    }
+}
